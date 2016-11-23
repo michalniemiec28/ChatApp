c96913b49b47704b3e6d1f49e42870e77341252a
Michał Niemiec <michalniemiec28@gmail.com>
2016-11-23 11:54:48 +0100

    asdf

--- lib/pipes/string_to_date_pipe.dart
+++
@@ -1,6 +0,0 @@
-import 'package:angular2/angular2.dart';
-
-@Pipe(name: 'stringToDate')
-class StringToDatePipe extends PipeTransform {
-  DateTime transform(String value) => DateTime.parse(value);
-}
--- web/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:angular2/platform/browser.dart';
-
-import 'package:DartChatApp/views/app_component/app_component.dart';
-
-main() {
-  bootstrap(AppComponent);
-}
